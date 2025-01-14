11bff98101749fc482dc4951ad378b46334bf8a0
Kevin Andres <kevinandres@ieee.org>
2025-01-14 12:21:48 -0500

    Primer commit

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,18 @@
+import './CreateTodoButton.css';
+import React from 'react';
+
+
+function CreateTodoButton({ setOpenModal }) {
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={
+        () => {
+          setOpenModal(state => !state);
+        }
+      }
+    >+</button>
+  );
+}
+
+export { CreateTodoButton };
---
+++ src/TodoList/index.js
@@ -0,0 +1,13 @@
+import './TodoList.css';
+import React from 'react';
+
+
+function TodoList({ children }) {
+  return (
+    <ul className="TodoList">
+      {children}
+    </ul>
+  );
+}
+
+export { TodoList };
